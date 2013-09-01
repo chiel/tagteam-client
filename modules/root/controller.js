793edb2b9d0bf1e14ee7f5928ccb59e7fdeaede3
Chiel Kunkels <chiel@kunkels.me>
2013-09-02 00:45:34 +0200

    Added basic home page

--- modules/root/controller.js
+++ modules/root/controller.js
@@ -12,7 +12,24 @@ function index(req){
 		header: '/views/header.html',
 		footer: '/views/footer.html',
 		body: new Promise(function(resolve){
-			resolve('index');
+			var view, data;
+			if (!req.session.user){
+				view = '/views/forms/login.html';
+				data = {action: '/login'};
+			} else {
+				view = '/views/home.html';
+				data = {};
+			}
+
+			template.render(view, data).then(
+				function(rendered){
+					resolve(rendered);
+				},
+				function(error){
+					console.log(error);
+					resolve('something wrong rendering');
+				}
+			);
 		})
 	};
 }
